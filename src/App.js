9ee123a8d24825f08861e1b89a65594d475cc5de
Nathaniel Alba <nathaniel.alba@gmail.com>
2017-11-09 16:55:19 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react'
+import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import getWeb3 from './utils/getWeb3'
+
+import './css/oswald.css'
+import './css/open-sans.css'
+import './css/pure-min.css'
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      storageValue: 0,
+      web3: null,
+      user: null,
+      name: '',
+    };
+  }
+
+  componentWillMount() {
+    // Get network provider and web3 instance.
+    // See utils/getWeb3 for more info.
+
+    getWeb3
+    .then(results => {
+      this.setState({
+        web3: results.web3
+      })
+
+      // Instantiate contract once web3 provided.
+      this.instantiateContract()
+    })
+    .catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
+
+  instantiateContract() {
+    /*
+     * SMART CONTRACT EXAMPLE
+     *
+     * Normally these functions would be called in the context of a
+     * state management library, but for convenience I've placed them here.
+     */
+
+    const contract = require('truffle-contract')
+    const simpleStorage = contract(SimpleStorageContract)
+    simpleStorage.setProvider(this.state.web3.currentProvider)
+
+    // Declaring this for later so we can chain functions on SimpleStorage.
+    var simpleStorageInstance
+
+    // Get accounts.
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      simpleStorage.deployed().then((instance) => {
+        simpleStorageInstance = instance
+
+        // Stores a given value, 5 by default.
+        return simpleStorageInstance.set(5, {from: accounts[0]})
+      }).then((result) => {
+        // Get the value from the contract to prove it worked.
+        return simpleStorageInstance.get.call(accounts[0])
+      }).then((result) => {
+        // Update state with the result.
+        return this.setState({ storageValue: result.c[0] })
+      })
+    })
+  }
+
+  submitName() {
+    const {
+      name,
+    } = this.state;
+
+    if (name.length > 0) {
+      this.generateWallet();
+    }
+
+    return;
+  }
+
+  renderCreateUser() {
+    return (
+      <main className="container">
+        <div className="pure-g">
+          <form className="pure-form">
+            <fieldset>
+              <input
+                type="text"
+                placeholder="Name"
+                value={this.state.name}
+                onChange={val => this.setState({ name: val.target.value  })}
+              />
+
+              <button
+                type="submite"
+                className="pure-button pure-button-primary"
+                onClick={this.submitName}
+              >
+                Enter Bar
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </main>
+    );
+  }
+
+  renderBar() {
+    return (
+      <main className="container">
+        <div className="pure-g">
+          <h1>Here is the bar</h1>
+        </div>
+      </main>
+    );
+  }
+
+  render() {
+    const {
+      user,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <nav className="navbar pure-menu pure-menu-horizontal">
+            <a href="#" className="pure-menu-heading pure-menu-link">ethereum BAR</a>
+        </nav>
+
+        { (user != null) ? this.renderBar() : this.renderCreateUser() }
+      </div>
+    );
+  }
+}
+
+export default App
